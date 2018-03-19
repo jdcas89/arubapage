81cf8253ba87cf7f5a85b57509a33b9960159efc
Sitelift <robingiel@gmail.com>
2018-03-19 20:25:30 +0100

    Analytics in place and working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Favicon from 'react-favicon';
+import Analytics from 'react-router-ga';
 import ico from './images/aruba.ico';
 import './App.css';
-
 import Home from './pages/Home.jsx';
 import About from './pages/About.jsx';
 
@@ -11,11 +11,13 @@ class App extends Component {
   render() {
       return (
         <Router>
+          <Analytics id="UA-115970603-1" debug>
           <div>
             <Favicon url={[ico]} />
             <Route exact path="/" component={Home}/>
             <Route exact path="/about" component={About} />
           </div>
+          </Analytics>
         </Router>
       );
   }
